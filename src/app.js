f0e7cd72deb0ebc18ae6e768aa5c32b51f452e9b
Clement MASSARD <cmassard@palo-it.com>
2018-08-10 17:07:22 +0200

    Set manual data binding

--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
 console.log('App.js is running...');
 
-var template = <h1>This is JSX from app.js.</h1>;
-var appRoot = document.getElementById('app');
+const appObject = {
+  title: 'Indecision App',
+  subtitle: 'Helps you decide what to do.',
+  options: ['One', 'Two']
+}
+// JSX - JavaScript XML
+const template = (
+  <div>
+    <h1>{appObject.title}</h1> 
+    {appObject.subtitle && <p>{appObject.subtitle}</p>}
+    <p>{appObject.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
 
-ReactDOM.render(template, appRoot);
+let count = 0;
+const addOne = () => {
+  count++;
+  renderCounterApp();
+};
+const subtractOne = () => {
+  if (count > 0) {
+    count--;
+    renderCounterApp();
+  }
+};
+const reset = () => {
+  count = 0;
+  renderCounterApp();
+};
+
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={subtractOne}>-1</button>
+      <button onClick={addOne}>+1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
