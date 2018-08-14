c5370f219ccad7ff7142edc3796e68df406e5e3e
Clement MASSARD <cmassard@palo-it.com>
2018-08-14 17:13:56 +0200

    Lifecycle methods & save data locally

--- src/app.js
+++ src/app.js
@@ -6,8 +6,29 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: props.options
+      options: []
     };
+  }
+  componentDidMount() {
+    try {
+      const jsonData = localStorage.getItem('options');
+      const options = JSON.parse(jsonData);
+  
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const jsonData = JSON.stringify(this.state.options);
+      localStorage.setItem('options', jsonData);
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
   }
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
@@ -58,11 +79,6 @@ class IndecisionApp extends React.Component {
   }
 }
 
-// Default props for Header component
-IndecisionApp.defaultProps = {
-  options: []
-};
-
 // Stateless functional components
 const Header = (props) => {
   return (
@@ -96,6 +112,9 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && 
+        <p>Please add an option to get started.</p>
+      }
       {
         props.options.map((option) => (
           <Option 
@@ -139,9 +158,12 @@ class AddOption extends React.Component {
     const option = e.target.elements.option.value.trim();
 
     const error = this.props.handleAddOption(option);
-    e.target.elements.option.value = '';
 
     this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (
