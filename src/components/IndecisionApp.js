8279ef2f6c5535a3a7c29f4c66d570cb315ead7e
Clement MASSARD <cmassard@palo-it.com>
2018-08-16 16:32:51 +0200

    Source maps, webpack dev server & ES6 class properties

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,16 +6,33 @@ import Header from './Header';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component {
-  constructor (props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteSingleOption = this.handleDeleteSingleOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
+  state = {
+    options: []
+  };
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
+  handleDeleteSingleOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter(option => option !== optionToRemove)
+    }));
+  };
+  handlePick = () => {
+    const randomNumber = Math.floor(Math.random() * this.state.options.length);
+    const pick = this.state.options[randomNumber];
+    console.log('pick', pick);
+  };
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item.';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'Item already exists.';
+    }
+
+    this.setState((prevState) => ({ 
+      options: prevState.options.concat(option) 
+    }));
+  };
   componentDidMount() {
     try {
       const jsonData = localStorage.getItem('options');
@@ -36,30 +53,6 @@ export default class IndecisionApp extends React.Component {
   }
   componentWillUnmount() {
     console.log('componentWillUnmount');
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-  handleDeleteSingleOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter(option => option !== optionToRemove)
-    }));
-  }
-  handlePick() {
-    const randomNumber = Math.floor(Math.random() * this.state.options.length);
-    const pick = this.state.options[randomNumber];
-    console.log('pick', pick);
-  }
-  handleAddOption(option) {
-    if (!option) {
-      return 'Enter valid value to add item.';
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'Item already exists.';
-    }
-
-    this.setState((prevState) => ({ 
-      options: prevState.options.concat(option) 
-    }));
   }
   render() {
     const subtitle = 'Put your life in the hands of a computer';
