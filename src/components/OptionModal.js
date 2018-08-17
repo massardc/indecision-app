06b05e9b79d1f9570d21048d5db211d4f0c2f66d
Clement MASSARD <cmassard@palo-it.com>
2018-08-17 17:05:47 +0200

    Style React-Modal & make the app responsive

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,13 +6,15 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleCloseModal}
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
     ariaHideApp={false}
   >
-    <h3>Selected Option</h3>
+    <h3 className="modal__title">Selected Option</h3>
     {props.selectedOption && 
-      <p>{props.selectedOption}</p>
+      <p className="modal__body">{props.selectedOption}</p>
     }
-    <button onClick={props.handleCloseModal}>Okay</button>
+    <button className="button" onClick={props.handleCloseModal}>Okay</button>
   </Modal>
 );
 
